b43a54903f6890ca9d175f89704ac3b8dbdf7154
Giovanni Minghelli <105241670+giovannimin@users.noreply.github.com>
2023-08-08 16:22:28 +0200

    Update lib_string.py

--- src/lib_string.py
+++ src/lib_string.py
@@ -5,3 +5,30 @@ Created on 31/07/2023 18:37
 """
 
 fbref_search_url = "https://fbref.com/search/search.fcgi?search"
+
+convert_var_name_to_french = {'Non-Penalty Goals': 'Buts (sans les pénaltys)',
+                              'Non-Penalty xG': 'xG sans les pénaltys',
+                              'Shots Total': 'Total des tirs',
+                              'Assists': 'Passes décisives',
+                              'xAG': 'xAG',
+                              'npxG + xAG': 'npxG + xAG',
+                              'Shot-Creating Actions': 'Actions menant à un tir',
+                              'Passes Attempted': 'Passes tentées',
+                              'Pass Completion %': '% de passes réussies',
+                              'Progressive Passes': 'Passes progressives',
+                              'Progressive Carries': 'Possessions progressives',
+                              'Successful Take-Ons': 'Dribbles réussis',
+                              'Touches (Att Pen)': 'Touches (SurfRépOff)',
+                              'Progressive Passes Rec': 'Passes progressives reçues',
+                              'Tackles': 'Tacles',
+                              'Interceptions': 'Interceptions',
+                              'Blocks': 'Balles contrées',
+                              'Clearances': 'Dégagements',
+                              'Aerials won': 'Duels aérien gagnés'}
+
+
+convert_positions = {}
+
+convert_country = {}
+
+convert_foot = {}
